899edc99fa60c9fb63e9c40508f5ecbac353c64a
yfl <yangfulin123@qq.com>
2020-12-26 20:04:27 +0800

    first commit

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,62 @@
+<template>
+  <el-tree :data="menus" :props="defaultProps" :expand-on-click-node="false" show-checkbox node-key="catId">
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level <=2"
+            type="text"
+            size="mini"
+            @click="() => append(data)">
+            Append
+          </el-button>
+          <el-button
+            v-if="node.childNodes.length == 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)">
+            Delete
+          </el-button>
+        </span>
+      </span>
+  </el-tree>
+</template>
+
+<script>
+export default {
+  name: 'category',
+  data () {
+    return {
+      menus: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    }
+  },
+  methods: {
+    getMenus () {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree'),
+        method: 'get'
+      }).then(({data}) => {
+        console.log('成功获取到菜单数据', data.data)
+        this.menus = data.data
+      })
+    },
+    append (data) {
+      console.log('append', data)
+    },
+    remove (node, data) {
+      console.log('remove', node, data)
+    }
+  },
+  created () {
+    this.getMenus()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
