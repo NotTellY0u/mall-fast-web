7023d700a871bc705dc2c7e0141b7de8d26d0041
yfl <yangfulin123@qq.com>
2020-12-29 22:45:04 +0800

    新增提示

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,36 +1,28 @@
 <template>
   <div>
     <el-tree :data="menus" :props="defaultProps" :expand-on-click-node="false" show-checkbox node-key="catId"
-             default-expanded-keys="expandedKey">
+             :default-expanded-keys="expandedKey">
     <span class="custom-tree-node" slot-scope="{ node, data }">
         <span>{{ node.label }}</span>
         <span>
-          <el-button
-            v-if="node.level <=2"
-            type="text"
-            size="mini"
-            @click="() => append(data)">
+          <el-button v-if="node.level <=2" type="text" size="mini" @click="() => append(data)">
             Append
           </el-button>
-          <el-button
-            v-if="node.childNodes.length == 0"
-            type="text"
-            size="mini"
-            @click="() => remove(node, data)">
+          <el-button v-if="node.childNodes.length == 0" type="text" size="mini" @click="() => remove(node, data)">
             Delete
           </el-button>
         </span>
       </span>
     </el-tree>
-    <el-dialog
-      title="提示"
-      :visible.sync="dialogVisible"
-      width="30%"
-      :before-close="handleClose">
-      <span>这是一段信息</span>
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+      <el-form :model="category">
+        <el-form-item label="分类名称">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
       <span slot="footer" class="dialog-footer">
     <el-button @click="dialogVisible = false">取 消</el-button>
-    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+    <el-button type="primary" @click="addCategory">确 定</el-button>
   </span>
     </el-dialog>
   </div>
@@ -41,6 +33,7 @@ export default {
   name: 'category',
   data () {
     return {
+      category: {name: '', parentCid: 0, catLevel: 0, showStatus: 1, sort: 0},
       dialogVisible: false,
       expandedKey: [],
       menus: [],
@@ -61,9 +54,16 @@ export default {
       })
     },
     append (data) {
-      this.dialogVisible = true
       console.log('append', data)
+      this.dialogVisible = true
+      this.category.parentCid = data.catId
+      this.category.catLevel = data.catLevel * 1 + 1
     },
+    // 添加三级分类
+    addCategory () {
+      console.log('提交的三级分类数据', this.category)
+    },
+
     remove (node, data) {
       var ids = [data.catId]
       this.$confirm(`是否删除${data.name}菜单?`, '提示', {
