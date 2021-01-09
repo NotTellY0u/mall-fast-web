a5da6cd7a077792a8c5dd5bed0aa0fd5ee21b45e
yfl <yangfulin123@qq.com>
2021-01-09 22:33:17 +0800

    属性分组，前端组件抽取

---
+++ src/views/modules/common/category.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'category.vue'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/modules/product/attrgroup.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-row :gutter="20">
+    <el-col :span="6">
+      菜单
+    </el-col>
+    <el-col :span="18">
+      表格
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: 'attrgroup'
+}
+</script>
+
+<style scoped>
+
+</style>
