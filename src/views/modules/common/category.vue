f89ba1c3a1a2e5f8c01edee2d1950411ed15181e
yfl <yangfulin123@qq.com>
2021-01-10 14:00:54 +0800

    属性分类，前端组件抽取父子组件交互

--- src/views/modules/common/category.vue
+++ src/views/modules/common/category.vue
@@ -1,9 +1,39 @@
 <template>
-
+  <el-tree :data="menus" :props="defaultProps" node-key="catId" ref="menuTree" @node-click="nodeClick">
+  </el-tree>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      expandedKey: [],
+      menus: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    }
+  },
+  methods: {
+    getMenus () {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree'),
+        method: 'get'
+      }).then(({data}) => {
+        console.log('成功获取到菜单数据', data.data)
+        this.menus = data.data
+      })
+    },
+    nodeClick (data, node, component) {
+      console.log('子组件category的节点被点击', data, node, component)
+      this.$emit('tree-node-click', data, node, component)
+    }
+  },
+
+  created () {
+    this.getMenus()
+  },
   name: 'category.vue'
 }
 </script>
--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -75,7 +75,7 @@ export default {
   methods: {
     getMenus () {
       this.$http({
-        url: this.$http.adornUrl('/product/category/list/tree'),
+        url: this.$http.adornUrl('/product/category.vue/list/tree'),
         method: 'get'
       }).then(({data}) => {
         console.log('成功获取到菜单数据', data.data)
@@ -95,7 +95,7 @@ export default {
         type: 'warning'
       }).then(() => {
         this.$http({
-          url: this.$http.adornUrl('/product/category/delete'),
+          url: this.$http.adornUrl('/product/category.vue/delete'),
           method: 'post',
           data: this.$http.adornData(catIds, false)
         }).then(({data}) => {
@@ -110,7 +110,7 @@ export default {
     },
     batchSave () {
       this.$http({
-        url: this.$http.adornUrl('/product/category/update/sort'),
+        url: this.$http.adornUrl('/product/category.vue/update/sort'),
         method: 'post',
         data: this.$http.adornData(this.updateNodes, false)
       }).then(({data}) => {
@@ -240,7 +240,7 @@ export default {
     editCategory () {
       let {catId, name, icon, productUnit} = this.category
       this.$http({
-        url: this.$http.adornUrl('/product/category/update'),
+        url: this.$http.adornUrl('/product/category.vue/update'),
         method: 'post',
         data: this.$http.adornData({catId: catId, name: name, icon: icon, productUnit: productUnit}, false)
       }).then(({data}) => {
@@ -257,7 +257,7 @@ export default {
     addCategory () {
       console.log('提交的三级分类数据', this.category)
       this.$http({
-        url: this.$http.adornUrl('/product/category/save'),
+        url: this.$http.adornUrl('/product/category.vue/save'),
         method: 'post',
         data: this.$http.adornData(this.category, false)
       }).then(({data}) => {
@@ -280,7 +280,7 @@ export default {
         type: 'warning'
       }).then(() => {
         this.$http({
-          url: this.$http.adornUrl('/product/category/delete'),
+          url: this.$http.adornUrl('/product/category.vue/delete'),
           method: 'post',
           data: this.$http.adornData(ids, false)
         }).then(({data}) => {
